d19381d00778fb966156315e9a44dd3bd24c0a41
Vinh825 <vinhthtce181492@fpt.edu.vn>
2026-02-14 20:22:36 +0700

    Delete patient directory

--- patient/src/main/java/com/healthcare/patient/iam/dto/IamAuthResponse.java
+++
@@ -1,6 +0,0 @@
-package com.healthcare.patient.iam.dto;
-
-import com.healthcare.patient.common.BaseSingleResultResponse;
-
-// File này y hệt như project patienttestorder
-public class IamAuthResponse extends BaseSingleResultResponse<IamAuthResponseData> { }
--- patient/src/main/java/com/healthcare/patient/repository/TestOrderAuditLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.healthcare.patient.repository;
-
-import com.healthcare.patient.entity.TestOrderAuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestOrderAuditLogRepository extends JpaRepository<TestOrderAuditLog, Long> {
-}
